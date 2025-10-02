7ee8c1c0b281d9057ba9fb29bd87f53c21051ee8
qureshisahil <sahil99.qur@gmail.com>
2025-10-02 18:06:59 -0400

    landing page Update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import SplashCursor from './components/SplashCursor';
-import NavBar from './components/NavBar'; // Import the new NavBar component
-import './App.css'; 
+import NavBar from './components/NavBar';
+import DarkVeil from './components/DarkVeil';
+import GlassSurface from './components/GlassSurface';
+import TextPressure from './components/TextPressure';
+import './App.css';
 
 function App() {
   return (
     <div className="homepage">
-      {/* The NavBar component goes here */}
-      <NavBar />
-      <SplashCursor />
+      {/* The DarkVeil component is the lowest layer (z-index: 0) */}
+      <DarkVeil /> 
       
-      <header className="homepage-content">
-        <h1>Hello, I'm Sahil Shakib Qureshi</h1>
-        <p>Associate Product Manager & Software Engineer</p>
-        {/* Your content will be added below this header */}
-      </header>
+      {/* The SplashCursor sits on top of the DarkVeil (z-index: 50) */}
+      <SplashCursor />
+
+      {/* The GlassSurface for your name sits on top of the cursor to blur the effect (z-index: 60) */}
+      <div className="center-panel-container">
+        <GlassSurface
+          width={800} 
+          height={300} 
+          borderRadius={25}
+          className="main-content-glass"
+        >
+          <TextPressure
+            text="Sahil Qureshi"
+            minFontSize={50}
+            flex={true}
+            stroke={false}
+            className="text-pressure-main"
+            textColor="#ffffff"
+          />
+        </GlassSurface>
+        <header className="homepage-content">
+          <p>Associate Product Manager & Software Engineer</p>
+        </header>
+      </div>
+
+      {/* The NavBar sits on the highest layer (z-index: 100) */}
+      <NavBar />
     </div>
   );
 }
